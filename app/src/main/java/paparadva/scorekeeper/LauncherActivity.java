55406aea4944910623d25e1a87c4d0231fa05bd4
paparadva <tsyroarseniy@gmail.com>
2017-08-30 15:04:18 +0300

    Return ArrayList of player names from PlayerNamesActivity

--- app/src/main/java/paparadva/scorekeeper/LauncherActivity.java
+++ app/src/main/java/paparadva/scorekeeper/LauncherActivity.java
@@ -3,13 +3,19 @@ package paparadva.scorekeeper;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class LauncherActivity extends AppCompatActivity {
+    private static final String TAG = LauncherActivity.class.getSimpleName();
+
     private static final int REQUEST_PLAYER_NUMBER = 0;
     private static final int REQUEST_PLAYER_NAMES = 1;
 
     public static final String EXTRA_PLAYER_NUMBER = "paparadva.scorekeeper.PLAYER_NUMBER";
+    public static final String EXTRA_PLAYER_NAMES = "paparadva.scorekeeper.PLAYER_NAMES";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +37,12 @@ public class LauncherActivity extends AppCompatActivity {
                     getNames.putExtra(EXTRA_PLAYER_NUMBER, playerNum);
 
                     startActivityForResult(getNames, REQUEST_PLAYER_NAMES);
+                    break;
+                }
+                case REQUEST_PLAYER_NAMES: {
+                    ArrayList<String> names = data.getStringArrayListExtra(EXTRA_PLAYER_NAMES);
+                    Log.d(TAG, names.toString());
+                    break;
                 }
             }
         }
--- app/src/main/java/paparadva/scorekeeper/PlayerNamesActivity.java
+++ app/src/main/java/paparadva/scorekeeper/PlayerNamesActivity.java
@@ -1,5 +1,6 @@
 package paparadva.scorekeeper;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,14 +8,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PlayerNamesActivity extends AppCompatActivity {
 
     private RecyclerView mNameInputs;
+    private int mPlayerNumber;
+    private PlayerNamesAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +33,7 @@ public class PlayerNamesActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        int playerNumber = getIntent().getIntExtra(LauncherActivity.EXTRA_PLAYER_NUMBER, 0);
+        mPlayerNumber = getIntent().getIntExtra(LauncherActivity.EXTRA_PLAYER_NUMBER, 0);
 
         mNameInputs = (RecyclerView) findViewById(R.id.rv_name_inputs);
         mNameInputs.setHasFixedSize(true);
@@ -32,42 +42,75 @@ public class PlayerNamesActivity extends AppCompatActivity {
                 this, LinearLayoutManager.VERTICAL, false);
         mNameInputs.setLayoutManager(layoutManager);
 
-        mNameInputs.setAdapter(
-                new PlayerNamesAdapter(playerNumber, getString(R.string.enter_name_label))
-        );
+        mAdapter = new PlayerNamesAdapter(mPlayerNumber, getString(R.string.enter_name_label));
+        mNameInputs.setAdapter(mAdapter);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                finishWithResult();
             }
         });
+    }
+
+    private void finishWithResult() {
+        Intent namesData = new Intent();
+
+        ArrayList<String> names = mAdapter.getEnteredText();
+        namesData.putStringArrayListExtra(LauncherActivity.EXTRA_PLAYER_NAMES, names);
+
+        setResult(RESULT_OK, namesData);
+        finish();
     }
 
 }
 
 class PlayerNamesAdapter extends RecyclerView.Adapter<PlayerNamesAdapter.NameInputViewHolder> {
+    private ArrayList<String> mEnteredTextData;
     private int mPlayerNumber;
     private final String LABEL_FORMAT;
 
     class NameInputViewHolder extends RecyclerView.ViewHolder {
         private TextView playerLabel;
+        private EditText nameEdit;
 
         NameInputViewHolder(View view) {
             super(view);
             playerLabel = view.findViewById(R.id.tv_enter_name_label);
+            nameEdit = view.findViewById(R.id.et_player_name);
         }
 
-        void bind(int playerOrdinal) {
+        void bind(final int position) {
+            int playerOrdinal = position + 1;
             playerLabel.setText(String.format(LABEL_FORMAT, playerOrdinal));
+
+            nameEdit.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+                @Override
+                public void afterTextChanged(Editable editable) {}
+
+                @Override
+                public void onTextChanged(CharSequence s, int i, int i1, int i2) {
+                    mEnteredTextData.set(position, s.toString());
+                }
+            });
         }
     }
 
     PlayerNamesAdapter(int playerNumber, final String label_format) {
         LABEL_FORMAT = label_format;
         mPlayerNumber = playerNumber;
+        mEnteredTextData = new ArrayList<>(playerNumber);
+        for(int i=0; i<playerNumber; i++) {
+            mEnteredTextData.add("");
+        }
+    }
+
+    ArrayList<String> getEnteredText() {
+        return mEnteredTextData;
     }
 
     @Override
@@ -79,7 +122,7 @@ class PlayerNamesAdapter extends RecyclerView.Adapter<PlayerNamesAdapter.NameInp
 
     @Override
     public void onBindViewHolder(NameInputViewHolder holder, int position) {
-        holder.bind(position + 1);
+        holder.bind(position);
     }
 
     @Override
